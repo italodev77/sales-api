85693477a36f7e2a3364fd5ee5cad1710b071171
devitalo <italo_ariel_dev77@outlook.com>
2024-07-07 11:51:48 -0300

    feat: Created create session service

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import { compare, hash } from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+class CreateSessionsService {
+    public async execute({ email, password }: IRequest): Promise<User> {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const user = await usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new AppError('Incorrect email/password combination.', 401);
+        }
+        const passwordConfirmed = await compare(password, user.password);
+
+        if (!passwordConfirmed) {
+            throw new AppError('Incorrect email/password combination.', 401);
+        }
+
+        return user;
+    }
+}
+
+export default CreateSessionsService;

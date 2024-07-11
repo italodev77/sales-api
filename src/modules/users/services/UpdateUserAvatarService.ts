dde7b570301b507ce67b0a05ba6a5e1b4479a0a0
devitalo <italo_ariel_dev77@outlook.com>
2024-07-10 21:03:04 -0300

    feat: created service for update user avatar

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,47 @@
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface IRequest {
+    user_id: string;
+    avatarFileName: string;
+}
+class UpdateUserAvatarService {
+    public async execute({
+        avatarFileName,
+        user_id,
+    }: IRequest): Promise<User | undefined> {
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const user = await usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError('user not found');
+        }
+
+        if (user.avatar) {
+            const userAvatarFilePath = path.join(
+                uploadConfig.directory,
+                user.avatar,
+            );
+
+            const userAvatarFileExists =
+                await fs.promises.stat(userAvatarFilePath);
+
+            if (userAvatarFileExists) {
+                await fs.promises.unlink(userAvatarFilePath);
+            }
+        }
+
+        user.avatar = avatarFileName;
+
+        await usersRepository.save(user);
+        return user;
+    }
+}
+
+export default UpdateUserAvatarService;

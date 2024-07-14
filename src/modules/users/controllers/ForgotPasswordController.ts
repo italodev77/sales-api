c9d6ad184f401f30a686c84761cbb274120d8a08
devitalo <italo_ariel_dev77@outlook.com>
2024-07-14 18:23:53 -0300

    feat: Created Controller for forgot password

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class UsersControllers {
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { email } = request.body;
+
+        const sendForgotPasswordEmailService =
+            new SendForgotPasswordEmailService();
+
+        const user = await sendForgotPasswordEmailService.execute({
+            email,
+        });
+
+        return response.status(204).json();
+    }
+}
